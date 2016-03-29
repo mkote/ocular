f6fd5efade61604530e75ae8961f0d89f48fc9a5
emil3507 <emil3507@gmail.com>
2016-03-29 09:46:11 +0200

    mne csp implementation works

--- d606/featureselection/mnecsp.py
+++ d606/featureselection/mnecsp.py
@@ -1,7 +1,10 @@
 import mne
 from mne.decoding import CSP
 from d606.preprocessing.dataextractor import *
-
+from sklearn.pipeline import Pipeline  # noqa
+from sklearn.cross_validation import cross_val_score  # noqa
+from sklearn.svm import SVC  # noqa
+from sklearn.cross_validation import ShuffleSplit  # noqa
 
 # load data
 data = load_data(1, 'T')
@@ -18,16 +21,30 @@ ch_names += ['eeg' + str(x) for x in range(1, 23)]
 ch_types = ['eog' for x in range(0, 3)]
 ch_types += ['eeg' for x in range(1, 23)]
 
+# Create label info
+labels = csp_label_reformat(labels, 2)
+
 # Create data_info and event_info
 data_info = mne.create_info(ch_names, HERTZ, ch_types, None)
-event_info = create_events(trials, labels)
+event_info = create_events(labels)
 
 # Create mne structure
 epochs_data = mne.EpochsArray(d3_data, data_info, event_info)
 
-""" Do some crazy csp stuff"""
+""" Do some crazy csp stuff """
 
-# reformat labels list
-labels = csp_label_reformat(labels, 1)
-csp = CSP(n_components=4)
-csp.fit(d3_data, labels)
+# Cross validation with sklearn
+labels = epochs_data.events[:, -1]
+evoked = epochs_data.average()
+
+n_components = 3  # pick some components
+svc = SVC(C=1, kernel='linear')
+csp = CSP(n_components=n_components)
+
+# Define a monte-carlo cross-validation generator (reduce variance):
+cv = ShuffleSplit(len(labels), 10, test_size=0.2, random_state=42)
+epochs_extract_data = epochs_data.get_data()
+
+clf = Pipeline([('CSP', csp), ('SVC', svc)])
+scores = cross_val_score(clf, epochs_extract_data, labels, cv=cv, n_jobs=1)
+print(scores.mean())  # should match results above
