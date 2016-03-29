dc261ca95284fff6c086d80ebee3ccc640540fae
Kristian Hauge Jensen <kje13@student.aau.dk>
2016-03-29 10:14:08 +0200

    Filterbanks are working

--- d606/preprocessing/filter.py
+++ d606/preprocessing/filter.py
@@ -4,16 +4,16 @@ from d606.preprocessing.bandpass import bandpass_matrix
 class Filter:
     def __init__(self, range_spec):
         self._range = []
-        self.range = range_spec
+        self.band_range = range_spec
 
     def filter(self, data):
         filters = []
-        if len(self.range) <= 0:
+        if len(self.band_range) <= 0:
             ValueError("You dumb fuck, you need to specify a legal range")
         else:
-            for x in range(0, len(self.range)):
-                filters.append(bandpass_matrix(data, self.range[0][0],
-                                               self.range[0][1]))
+            for x in range(0, len(self.band_range)):
+                filters.append(bandpass_matrix(data, self.band_range[x][0],
+                                               self.band_range[x][1]))
             return filters
 
     def get_range(self):
@@ -22,4 +22,4 @@ class Filter:
     def set_range(self, val):
         self._range = val
 
-    range = property(get_range, set_range)
+    band_range = property(get_range, set_range)
