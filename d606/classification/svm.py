7461454746821bcc1913f9b54169cc54fb2710ae
Kristian Hauge Jensen <kje13@student.aau.dk>
2016-03-29 11:59:35 +0200

    Semi working multiclass csp

--- d606/classification/svm.py
+++ d606/classification/svm.py
@@ -1,14 +1,29 @@
-from sklearn import *
+from sklearn.svm import SVC
+from sklearn.cross_validation import ShuffleSplit
+from d606.preprocessing.dataextractor import d3_matrix_creator, csp_label_reformat
+from numpy import array, transpose
 
 
-# Note: if you want to plot, the input data should not be scaled.
-# otherwise, we should perform some scaling/normalization
-# before we apply the data on SVM.
-# This svm learner function learns a polynomial svm.
-# The standard regularization parameter is 1.0
+def train_svc(shuffle, csp, data, labels):
+    cv = shuffle
+    svc = SVC(C=1, kernel='linear')
+    for train_idx, test_idx in cv:
+        y_train, y_test = labels[train_idx], labels[test_idx]
+
+        X_train = csp.transform(data[train_idx])
+
+        # fit classifier
+        svc.fit(X_train, y_train)
+
+    return svc
 
 
-def learn_svm(self, feature_values, target_values, poly_degree, reg_param):
-    poly_svc = svm.SVC(kernel='poly', degree=poly_degree, C=reg_param)\
-        .fit(feature_values, target_values)
-    return poly_svc
+def csv_one_versus_all(csp_list, band):
+    data, trials, labels = band
+    d3_data = d3_matrix_creator(data)
+    svc_list = []
+    cv = ShuffleSplit(len(labels), 10, test_size=0.2, random_state=42)
+    for i, csp in enumerate(csp_list):
+        formatted_labels = array(csp_label_reformat(labels, i+1))
+        svc_list.append(train_svc(cv, csp, d3_data, formatted_labels))
+    return svc_list
--- d606/main.py
+++ d606/main.py
@@ -1,6 +1,8 @@
 from d606.preprocessing.dataextractor import *
 from d606.preprocessing.filter import Filter
 from d606.featureselection.mnecsp import *
+from d606.classification.svm import csv_one_versus_all
+from numpy import array
 
 
 filt = Filter([[8, 12], [12, 24]])
@@ -13,4 +15,12 @@ for eeg_signal in banks:
     bands.append((matrix, new_trials, labels))
 
 csp_list = csp_one_vs_all(bands[0], 4)
+
+svc_list = csv_one_versus_all(csp_list, bands[0])
+temp = []
+results = []
+temp.append(bands[0][0][0:25, 1500:2250])
+for svc, csp in zip(svc_list, csp_list):
+    transformed = csp.transform(array(temp))
+    results.append(svc.predict(transformed))
 print "Kristian"
