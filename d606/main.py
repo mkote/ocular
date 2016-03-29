91e77c421970cba2e899c7d167661f129316d378
Kristian Hauge Jensen <kje13@student.aau.dk>
2016-03-29 10:42:31 +0200

    Updated main

--- d606/main.py
+++ d606/main.py
@@ -1,5 +1,6 @@
 from d606.preprocessing.dataextractor import *
 from d606.preprocessing.filter import Filter
+from d606.featureselection.mnecsp import *
 
 
 filt = Filter([[8, 12], [12, 24]])
@@ -9,5 +10,7 @@ banks = filt.filter(data)
 matrices = []
 for eeg_signal in banks:
     matrix, new_trials = extract_trials(eeg_signal, trials)
-    matrices.append((matrix, new_trials))
-print "tro"
+    matrices.append((matrix, new_trials, labels))
+for band in matrices:
+    result = run_csp(band)
+    print result
