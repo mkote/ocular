8274c814feb779b67d4e9854b301050be4b53248
Bahm <x.benjamin.ahm@gmail.com>
2016-04-07 15:57:09 +0200

    adaptive thresholding

--- d606/preprocessing/adaptivethresholding.py
+++ d606/preprocessing/adaptivethresholding.py
@@ -10,18 +10,26 @@ NUM_SAMPLES = 64
 MAX = pywt.swt_max_level(NUM_SAMPLES)
 
 
+# http://deeplearning.net/software/theano/library/gradient.html
+
 # http://iopscience.iop.org/article/10.1088/1741-2560/3/4/011/meta
-# http://www.ee.ryerson.ca/~xzhang/publications/surespl.pdf
+# http://ieeexplore.ieee.org/xpl/articleDetails.jsp?arnumber=720560
 def softlike_threshold(v, t, k=3):
     for i in xrange(len(v)):
-        y = v[i]
-        if(y < -t):
-            v[i] = y + t - t / (2*k+1)
-        elif abs(y) <= t:
-            v[i] = (1/((2*k+1)*pow(t, 2*k))) * pow(y, 2*k+1)
-        elif y > t:
-            v[i] = y - t + t / (2*k+1)
+        softlike_threshold_aux(v[i], t, k)
     return v
+
+
+def softlike_threshold_aux(y, t, k=3):
+    if (y < -t):
+        y = y + t - t / (2 * k + 1)
+    elif abs(y) <= t:
+        y = (1 / ((2 * k + 1) * pow(t, 2 * k))) * pow(y, 2 * k + 1)
+    elif y > t:
+        y = y - t + t / (2 * k + 1)
+    return y
+
+
 
 data = []
 for i in range(0, NUM_SAMPLES):
@@ -30,17 +38,22 @@ for i in range(0, NUM_SAMPLES):
 # MAX = the number of times NUM_SAMPLES is divisible by 2
 print(MAX)
 
+wt = pywt.swt(data, 'sym3', MAX)
+
 for j in xrange(MAX):
-    wt = pywt.swt(data, 'sym3', MAX)
     lvlj_approx = wt[j][0]
     lvlj_details = wt[j][1]
 
     sigma = median(lvlj_details) / 0.6745
     threshold = math.sqrt((2 * pow(sigma, 2)) * math.log(NUM_SAMPLES))
-    lvlj_details = softlike_threshold(lvlj_details, threshold, 3)
+    lvlj_details_after = softlike_threshold(lvlj_details, threshold, 3)
+
+    gj = lvlj_details_after - lvlj_details
+
+
 
     wt[j] = list(wt[j])
-    wt[j][1] = lvlj_details
+    wt[j][1] = lvlj_details_after
 
     #wt = _thresholding.less(wt, 1.7)
     iwt = pywt.iswt(wt, 'sym3')
