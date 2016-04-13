4784918c827cd3a3193425c450a18b608243ba5b
Kristian Hauge Jensen <kje13@student.aau.dk>
2016-04-14 01:46:39 +0200

    Did the multi processing transform

--- d606/preprocessing/oacl.py
+++ d606/preprocessing/oacl.py
@@ -267,7 +267,7 @@ def get_theta(raw_signal, trials_start, labels, range_list, m):
     filtering_param = np.array([[min_result.x[k]] for k in xrange(len(min_result.x) - 1)])
     # b = min_result.x[len(min_result.x) - 1]
 
-    return filtering_param
+    return filtering_param, artifact_signals
 
 
 def clean_signal(data, thetas, params):
@@ -281,18 +281,47 @@ def clean_signal(data, thetas, params):
     return cleaned_signal
 
 
+def clean_signal_multiproc(input_q, output_q, thetas, params):
+    range_list, m, decimal_precision, trials = params
+    getcontext().prec = decimal_precision
+    if trials is True:
+        data, artifacts_signals, index = input_q.get()
+    else:
+        data, index = input_q.get()
+    channels, trials, labels, artifacts = data
+    cleaned_signal = []
+    for i, (channel, theta) in enumerate(zip(channels, thetas)):
+        print "Process " + str(index) + " is cleaning " + str(i)
+        if trials is True:
+            artifact_signal = artifacts_signals[i]
+        else:
+            artifact_signal = find_artifact_signals(channel, m, range_list)
+        cleaned_signal.append(remove_ocular_artifacts(channel, theta, artifact_signal))
+    if not output_q.full():
+        output_q.put((cleaned_signal, index))
+        output_q.close()
+        output_q.join_thread()
+    else:
+        print "QUEUE IS FULL ????"
+    print "Process " + str(index) + " exiting!!"
+
+
 def estimate_theta_multiproc(input_q, output_q, params):
-    range_list, m, decimal_precision = params
+    range_list, m, decimal_precision, trials = params
     getcontext().prec = decimal_precision
     eeg_data, index = input_q.get()
     print "Process " + str(index) + " is starting"
     channels, trials_start, labels, artifacts = eeg_data
     clean_signals = []
-    for raw_signal in channels:
-        clean_signals.append(get_theta(raw_signal, trials_start, labels, range_list, m))
+    artifact_signals = []
+    for i, raw_signal in enumerate(channels):
+        print "Process " + str(index) + " is estimating channel " + str(i)
+        theta, artifact_signal = get_theta(raw_signal, trials_start, labels, range_list, m)
+        clean_signals.append(theta)
+        artifact_signals.append(artifact_signal)
 
     if not output_q.full():
-        output_q.put((clean_signals, index))
+        output_q.put((clean_signals, artifact_signals, index))
         output_q.close()
         output_q.join_thread()
     else:
