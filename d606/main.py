9123d5aed815609c113f82e76394d03b50c0638c
Bahm <x.benjamin.ahm@gmail.com>
2016-05-03 09:56:18 +0200

    MIFS done
    NEXT: cleanup

--- d606/main.py
+++ d606/main.py
@@ -89,6 +89,9 @@ def main(*args):
 
     sh = cross_validation.ShuffleSplit(6, n_iter=6, test_size=0.16)
 
+    thetas = []
+
+    accuracies = []
     for train_index, test_index in sh:
         csp_list = []
 
@@ -103,6 +106,7 @@ def main(*args):
         ranges = search.grid.oacl_ranges if 'oacl_ranges' in search.grid._fields else ((3, 7), (7, 15))
         oacl = OACL(ranges=ranges, m=m, multi_run=True)
         oacl.theta = oacl.generalize_thetas(array(thetas)[train_index])
+
         test = remake_single_run_transform(test, oacl)
 
         filt = search.grid.band_list if 'band_list' in search.grid._fields else [[8, 12], [16, 24]]
@@ -186,82 +190,14 @@ def main(*args):
             predictions.append(idx + 1)
 
         accuracy = np.mean([1 if a == b else 0 for (a, b) in zip(predictions, test_combined_labels)])
-
         print("Accuracy: " + str(accuracy) + "%")
 
+        accuracies.append(accuracy)
 
-        print "Done so far"
+    return np.mean(accuracies) * 100
 
 
-    # os.chdir(old_path)
-    #
-    # with timed_block('All Time'):
-    #     # for subject in [int(x) for x in range(1, 2)]:
-    #     csp_list = []
-    #     svc_list = []
-    #     rfl_list = []
-    #     filt = search.grid.band_list if 'band_list' in search.grid._fields else [[8, 12], [16, 24]]
-    #     filters = Filter(filt)
-    #
-    #     train_bands, train_combined_labels = restructure_data(runs, filters)
-    #
-    #     test_bands, test_combined_labels = restructure_data(evals, filters)
-    #
-    #     # CSP one VS all, give csp_one_cs_all num_different labels as input
-    #
-    #
-    #     # Create a svm for each csp and band
-    #     for csp, band in zip(csp_list, train_bands):
-    #         svc_list.append(csv_one_versus_all(csp, band))
-    #
-    #     # Create a random forest tree for each csp and band
-    #     for csp, band in zip(csp_list, train_bands):
-    #         rfl_list.append(rfl_one_versus_all(csp, band))
-    #
-    #     # Predict results with svm's
-    #     svm_results = svm_prediction(test_bands, svc_list, csp_list)
-    #
-    #     # Predict results with svm's
-    #     rfl_results = rfl_prediction(test_bands, rfl_list, csp_list)
-    #
-    #     svm_voting_results = csp_voting(svm_results)
-    #     rfl_voting_results = csp_voting(rfl_results)
-    #
-    #     svm_score, wrong_list = scoring(svm_voting_results, test_combined_labels)
-    #     rfl_score, wrong_list = scoring(rfl_voting_results, test_combined_labels)
-    #     score = svm_score if svm_score >= rfl_score else rfl_score
-    # print search.grid
-    # print 'rfl results: ' + str(rfl_score)
-    # print 'svm results ' + str(svm_score)
-    # print '\n'
-    # print 'Best score: ' + str(score)
-    # return svm_score, 1200
 
 if __name__ == '__main__':
     freeze_support()
     main(2, 0.1, 'rbf', [[4, 8], [8, 12], [12, 16], [16, 20], [20, 30]], ((3, 7), (7, 15)), 11)
-
-
-def check_selection(selected, i, r):
-    """
-    Check FN, FP, TP ratios among the selected features.
-    """
-    # reorder selected features
-    try:
-        selected = set(selected)
-        all_f = set(range(i+r))
-        TP = len(selected.intersection(all_f))
-        FP = len(selected - all_f)
-        FN =  len(all_f - selected)
-        if (TP+FN) > 0:
-            sens = TP/float(TP + FN)
-        else:
-            sens = np.nan
-        if (TP+FP) > 0:
-            prec =  TP/float(TP + FP)
-        else:
-            prec = np.nan
-    except:
-        sens = np.nan
-        prec = np.nan
-    return sens, prec
