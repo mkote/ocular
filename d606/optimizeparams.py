047b63475457f79fd4f52ff9a80e4497cb98f20c
Kristian Hauge Jensen <kje13@student.aau.dk>
2016-05-10 14:44:53 +0200

    Fix

--- d606/optimizeparams.py
+++ d606/optimizeparams.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
-import time
 from sys import executable
-from main import main
+from time import sleep
+from main import main, main_without_oacl
 from multiprocessing import freeze_support
 
 SVC_KERNELS = ['linear', 'rbf', 'poly']
@@ -13,17 +13,10 @@ NUM_ITERATIONS = 200
 
 def optim_params():
     for subject in range(FIRST_SUBJECT, LAST_SUBJECT + FIRST_SUBJECT):
-        if os.path.isfile('braninpy/chooser.GPEIOptChooser.pkl'):
-            os.remove('braninpy/chooser.GPEIOptChooser.pkl')
-        if os.path.isfile('braninpy/chooser.GPEIOptChooser_hyperparameters.txt'):
-            os.remove('braninpy/chooser.GPEIOptChooser_hyperparameters.txt')
-        if os.path.isfile('braninpy/results.dat'):
-            os.rename('braninpy/results.dat', 'braninpy/results.dat.' + str(time.time()))
-
         for iteration in range(NUM_ITERATIONS):
             old_path = os.getcwd()
             os.chdir('spearmintlite')
-            subprocess.check_call([executable, 'spearmintlite.py', '../braninpy'], cwd=os.getcwd())
+            p = subprocess.check_call([executable, 'spearmintlite.py', '../braninpy'], cwd=os.getcwd())
 
             os.chdir(old_path)
 
@@ -33,12 +26,19 @@ def optim_params():
             band_range = int(par[3])
             num_bands = int(36/band_range)
             band_list = [[4 + band_range * x, 4 + band_range * (x + 1)] for x in range(num_bands)]
+            s = int(par[4])
+            r1 = int(par[5])
+            r2 = int(par[6])
+            space = int(par[7])
+            m = int(par[8]) * 2 + 1
+            oacl_ranges = ((s, s + r1), (space + s + r1 + 1, space + s + r1 + 1 + r2))
 
-            result, timestamp = main(n_comp, band_list, subject)
+            result, time = main(n_comp, band_list, oacl_ranges, m, subject)
 
-            insert_result(result, timestamp)
-
+            insert_result(result, time)
         os.rename('braninpy/results.dat', 'braninpy/results' + str(subject) + '.dat')
+        os.remove('braninpy/chooser.GPEIOptChooser.pkl')
+        os.remove('braninpy/chooser.GPEIOptChooser_hyperparameters.txt')
 
 
 def get_params():
