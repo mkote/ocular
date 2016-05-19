242247ffde72820711cf4b7e6189e89919363537
Morten <motn92@gmail.com>
2016-05-19 12:53:31 +0200

    figgenfigures

--- d606/genfigures.py
+++ d606/genfigures.py
@@ -9,6 +9,9 @@ def plot_example1():
     # load the data.
     eeg_data = load_data(1, 'T')
     raw_signal = eeg_data[3][0][4]
+    eog_signal1 = eeg_data[3][0][22]
+    eog_signal2 = eeg_data[3][0][23]
+    eog_signal3 = eeg_data[3][0][24]
     #labels = eeg_data[5][2]
     #n_trials = 48
     m = 11
@@ -19,19 +22,22 @@ def plot_example1():
     padded_fsignal= [0]*(m/2)
     padded_fsignal.extend(filtered)
     rh, zero_indexes = find_relative_heights(filtered)
-    ti = find_peak_indexes(rh, (2, 3))
+    ti = find_peak_indexes(rh, (3, 5))
 
     artifacts = [0]*(m/2)
     a = find_artifact_signal(ti, filtered, zero_indexes)
     artifacts.extend(a)
     num_samples = len(raw)
     # prepare plot
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(2, sharex=True)
     #plt.subplot(211)
     # plot shit
-    ax.plot([x for x in range(0, len(raw_signal))], raw_signal, 'b--', label='raw signal: x(t)')
-    ax.plot([x for x in range(0, len(padded_fsignal))], padded_fsignal, 'g', label='smoothed signal: s(t)')
-    ax.plot([x for x in range(0, len(artifacts))], artifacts, 'r', label='artifacts: a(t)')
+    ax[0].plot([x for x in range(0, len(raw_signal))], raw_signal, 'b--', label='raw signal: x(t)')
+    ax[0].plot([x for x in range(0, len(padded_fsignal))], padded_fsignal, 'g', label='smoothed signal: s(t)')
+    ax[0].plot([x for x in range(0, len(artifacts))], artifacts, 'r', label='artifacts: a(t)')
+    ax[1].plot([x for x in range(0, len(eog_signal1))], eog_signal1, 'r', label='eog:')
+    ax[1].plot([x for x in range(0, len(eog_signal2))], eog_signal2, 'b', label='eog:')
+    ax[1].plot([x for x in range(0, len(eog_signal3))], eog_signal3, 'g', label='eog:')
     plt.legend(bbox_to_anchor=(0., 1.02, 1., .102),loc=3,ncol=3,mode="expand",borderaxespad=0)
     plt.ylabel('amplitude')
     plt.xlabel('time (t)')
